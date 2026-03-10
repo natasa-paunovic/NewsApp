2ed9b36fe09c25afa727307c8b01f1d531decdfd
borne <borne>
2026-03-10 20:54:22 +0100

    Integrated Basic Firebase Analytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -73,6 +74,11 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.androidx.navigation.compose)
     implementation (libs.bundles.koin)
+
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 
 
     testImplementation(libs.junit)
